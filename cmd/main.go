8b6153ee4113c12a03c397d0d16b6c7cd17759ac
Andrii Rakutskyi <andru.rakutsky@gmail.com>
2025-07-06 14:00:44 +0300

    Add controller

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,14 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"study/go-fiber/internal/home"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	home.NewHandler(app)
 	app.Listen(":3000")
 
 }
